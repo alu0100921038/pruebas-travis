6f8caa0629d8703b8637c6e68fd25fd31bc4a45d
Juan M <alu0100921038@ull.edu.es>
2019-11-11 09:46:00 +0000

    Comprobación correcta inicialización clase Alimento

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,6 +1,15 @@
 require "tdd/version"
 
-module Tdd
-  class Error < StandardError; end
-  # Your code goes here...
+class Alimento
+    attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :gei, :terreno
+
+    def initialize (nombre, proteinas, carbohidratos, lipidos, gei, terreno)
+      @nombre = nombre
+      @proteinas = proteinas
+      @carbohidratos = carbohidratos
+      @lipidos = lipidos
+      @gei = gei
+      @terreno = terreno
+    end
+
 end
