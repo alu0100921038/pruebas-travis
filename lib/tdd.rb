951411ff0ec98aa607d8f521006f38d7c9711bfc
Juan M <alu0100921038@ull.edu.es>
2019-11-11 09:03:23 +0000

    Estructura creada con bundler

---
+++ lib/tdd.rb
@@ -0,0 +1,6 @@
+require "tdd/version"
+
+module Tdd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tdd_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Tdd do
+  it "has a version number" do
+    expect(Tdd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
