4f5512100a0bdcbc9e5b292f6e1c2a2a4ee4fae9
Juan M <alu0100921038@ull.edu.es>
2019-11-11 09:30:14 +0000

    Primera expectativa creada. Gemspec modificado. RakeFile creado

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,9 +1,14 @@
+require "spec_helper"
+require 'lib/tdd'
+
 RSpec.describe Tdd do
-  it "has a version number" do
-    expect(Tdd::VERSION).not_to be nil
+  before :each do
+    @alimento1 = Alimento.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "El alimento se inicializa correctamente y se puede acceder a sus atributos" do
+    it "Debe existir un nombre para el alimento" do
+      expect(@alimento1.nombre).to eq("Chocolate")
+    end
   end
 end
